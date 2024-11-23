c82f145cc05d0d17e7c95934a0d564307412d96c
Chung-Ying <ag.cyho@gmail.com>
2024-11-23 21:39:32 +0800

    feat: Jeremy check

--- README.md
+++ README.md
@@ -1,43 +1,50 @@
 # 請檢查以下測試是否通過
+已知問題：
+1. 遞迴通常會按字母順序，但有時好像會錯亂。這算 bug 嗎？
+2. #14 發現，指定路徑下沒有 `.env` 跟 `.env.example` 時連錯都不會噴
+
 ## Jeremy
 ### 程式檢查
-- [ ] 1. 預設檢查
-- [ ] 2. 傳 `.env.sample` 跟 `.env.local`
-- [ ] 3. 只傳 `envName`: `.env.setting`
-- [ ] 4. 只傳 `schemaName`: `.env.schema`
-- [ ] 5. 只傳 `isCheckMissing`: `false`
-- [ ] 6. 只傳 `isCheckEmpty`: `false`
-- [ ] 7. 只傳 `isCheckExtra`: `false`
-- [ ] 8. 全傳 `false`
-- [ ] 9. 全傳 `true`
-- [ ] 10. 傳兩個 `false`
-- [ ] 11. `rootDir` 傳 `'application/project'`
-- [ ] 12. `rootDir` 傳不存在的資料夾
-- [ ] 13. `rootDir` 傳存在的資料夾
-- [ ] 14. `rootDir` 傳很深的資料夾
-- [ ] 15. `rootDir` 傳同名但不同路徑的資料夾
-- [ ] 16. schema 跟 env 檔案不在同一層
-- [ ] 17. schema 不存在
-- [ ] 18. env 不存在
-- [ ] 19. schema 跟 env 都不存在
+- [x] 1. 預設檢查：application 第一層通過，在檢查 backend 有抓到錯誤，有停住
+- [x] 2. 傳 `.env.sample` 跟 `.env.local`
+- [x] 3. 只傳 `envName`: `.env.setting`：前面檢查有沒 match 同層的 `.env.setting`，不知道該不該打 `process.exit(1)`
+
+![](./asset/3-test.png)
+
+- [x] 4. 只傳 `schemaName`: `.env.schema`
+- [x] 5. 只傳 `isCheckMissing`: `false`
+- [x] 6. 只傳 `isCheckEmpty`: `false`
+- [x] 7. 只傳 `isCheckExtra`: `false`
+- [x] 8. 全傳 `false`
+- [x] 9. 全傳 `true`
+- [x] 10. 傳兩個 `false`
+- [x] 11. `rootDir` 傳 `'application/project'`
+- [x] 12. `rootDir` 傳不存在的資料夾
+- [x] 13. `rootDir` 傳存在的資料夾
+- [x] 14. `rootDir` 傳很深的資料夾：已補 `.env` 跟 `.env.example`
+- [x] 15. `rootDir` 傳同名但不同路徑的資料夾：沒啥意義的檢查
+- [x] 16. schema 跟 env 檔案不在同一層
+- [ ] 17. schema 不存在：沒過，沒噴錯
+- [x] 18. env 不存在：錯誤顯示同 #16
+- [ ] 19. schema 跟 env 都不存在：沒過，沒噴錯
 
 ### CLI 檢查
-- [ ] 1. `npx env-aligner`
-- [ ] 2. `npx env-aligner -h`
-- [ ] 3. `npx env-aligner -v`
-- [ ] 4. `npx env-aligner -s .env.sample`
-- [ ] 5. `npx env-aligner -e .env.local`
-- [ ] 6. `npx env-aligner -s .env.sample -e .env.local`
-- [ ] 7. `npx env-aligner --schema .env.sample --env .env.local`
-- [ ] 8. `npx env-aligner -m false`
-- [ ] 9. `npx env-aligner -m true`
-- [ ] 10. `npx env-aligner -n false`
-- [ ] 11. `npx env-aligner -x true`
-- [ ] 12. `npx env-aligner -m false -n false -x false`
-- [ ] 13. `npx env-aligner -m true -n true -x true`
-- [ ] 14. `npx env-aligner --missing false --empty false --extra false`
-- [ ] 15. `npx env-aligner -e .env.setting -s .env.schema` - 顛倒看抓不抓得到
-- [ ] 16. `npx env-aligner -e .env.setting -s .env.schema -m false -n false -x false`
+- [x] 1. `npx env-aligner`
+- [x] 2. `npx env-aligner -h`
+- [x] 3. `npx env-aligner -v`
+- [x] 4. `npx env-aligner -s .env.sample`
+- [x] 5. `npx env-aligner -e .env.local`
+- [x] 6. `npx env-aligner -s .env.sample -e .env.local`
+- [x] 7. `npx env-aligner --schema .env.sample --env .env.local`
+- [x] 8. `npx env-aligner -m false`
+- [x] 9. `npx env-aligner -m true`
+- [x] 10. `npx env-aligner -n false`
+- [x] 11. `npx env-aligner -x false`
+- [x] 12. `npx env-aligner -m false -n false -x false`
+- [x] 13. `npx env-aligner -m true -n true -x true`
+- [x] 14. `npx env-aligner --missing false --empty false --extra false`
+- [x] 15. `npx env-aligner -e .env.setting -s .env.schema` - 顛倒看抓不抓得到
+- [x] 16. `npx env-aligner -e .env.setting -s .env.schema -m false -n false -x false`
 
 
 ## JuiCheng
@@ -74,7 +81,7 @@
 - [ ] 8. `npx env-aligner -m false`
 - [ ] 9. `npx env-aligner -m true`
 - [ ] 10. `npx env-aligner -n false`
-- [ ] 11. `npx env-aligner -x true`
+- [ ] 11. `npx env-aligner -x false`
 - [ ] 12. `npx env-aligner -m false -n false -x false`
 - [ ] 13. `npx env-aligner -m true -n true -x true`
 - [ ] 14. `npx env-aligner --missing false --empty false --extra false`
--- src/test.js
+++ src/test.js
@@ -1,121 +1,122 @@
 const envAligner = require('env-aligner')
 
 // test default options
-console.log('The 1st test: default options')
-envAligner()
+// console.log('The 1st test: default options')
+// envAligner()
 
 // test custom names
-console.log('The 2nd test: custom file names with .env.local and .env.sample')
-const test2names = {
-  schemaName: '.env.sample',
-  envName: '.env.local'
-}
-envAligner({fileNames: test2names})
+// console.log('The 2nd test: custom file names with .env.local and .env.sample')
+// const test2names = {
+//   schemaName: '.env.sample',
+//   envName: '.env.local'
+// }
+// envAligner({fileNames: test2names})
 
 // test custom names -2
-console.log('The 3rd test: custom file names with custom env file name')
-const test3names = {
-  envName: '.env.setting'
-}
-envAligner({fileNames: test3names})
+// console.log('The 3rd test: custom file names with custom env file name')
+// const test3names = {
+//   envName: '.env.setting'
+// }
+// envAligner({fileNames: test3names})
 
 // test custom names -3
-console.log('The 4th test: custom file names with custom schema file name')
-const test4names = {
-  schemaName: '.env.schema'
-}
-envAligner({fileNames: test4names})
+// console.log('The 4th test: custom file names with custom schema file name')
+// const test4names = {
+//   schemaName: '.env.schema'
+// }
+// envAligner({fileNames: test4names})
 
 // test custom options
-console.log('The 5th test: custom options with isCheckMissing false')
-const test3options = {
-  isCheckMissing: false
-}
-envAligner({checkOptions: test3options})
+// console.log('The 5th test: custom options with isCheckMissing false')
+// const test3options = {
+//   isCheckMissing: false
+// }
+// envAligner({checkOptions: test3options})
 
 // test custom options -2 
-console.log('The 6th test: custom options with isCheckEmptyValue false')
-const test4options = {
-  isCheckEmptyValue: false
-}
-envAligner({checkOptions: test4options})
+// console.log('The 6th test: custom options with isCheckEmptyValue false')
+// const test4options = {
+//   isCheckEmptyValue: false
+// }
+// envAligner({checkOptions: test4options})
 
 // test custom options -3
-console.log('The 7th test: custom options with isCheckExtra false')
-const test5options = {
-  isCheckExtra: false
-}
-envAligner({checkOptions: test5options})
+// console.log('The 7th test: custom options with isCheckExtra false')
+// const test5options = {
+//   isCheckExtra: false
+// }
+// envAligner({checkOptions: test5options})
 
 // test custom options -4
-console.log('The 8th test: custom options with all false')
-const test6options = {
-  isCheckMissing: false,
-  isCheckEmptyValue: false,
-  isCheckExtra: false
-}
-envAligner({checkOptions: test6options})
+// console.log('The 8th test: custom options with all false')
+// const test6options = {
+//   isCheckMissing: false,
+//   isCheckEmptyValue: false,
+//   isCheckExtra: false
+// }
+// envAligner({checkOptions: test6options})
 
 // test custom options -5
-console.log('The 9th test: custom options with all true')
-const test7options = {
-  isCheckMissing: true,
-  isCheckEmptyValue: true,
-  isCheckExtra: true
-}
-envAligner({checkOptions: test7options})
+// console.log('The 9th test: custom options with all true')
+// const test7options = {
+//   isCheckMissing: true,
+//   isCheckEmptyValue: true,
+//   isCheckExtra: true
+// }
+// envAligner({checkOptions: test7options})
 
 // test custom options -6
-console.log('The 10th test: custom options with two false')
-const test8options = {
-  isCheckMissing: false,
-  isCheckExtra: false
-}
-envAligner({checkOptions: test8options})
+// console.log('The 10th test: custom options with two false')
+// const test8options = {
+//   isCheckMissing: false,
+//   isCheckExtra: false
+// }
+// envAligner({checkOptions: test8options})
 
 // test rootDir setting
-console.log('The 11th test: custom rootDir as use application/project')
-envAligner({rootDir: 'use default'})
+// console.log('The 11th test: custom rootDir as use application/project')
+// envAligner({rootDir: 'application/project'})
 
 // test rootDir setting with folder not exist
-console.log('The 12th test: custom rootDir as not exist folder')
-envAligner({rootDir: 'not_exist_folder'})
+// console.log('The 12th test: custom rootDir as not exist folder')
+// envAligner({rootDir: 'not_exist_folder'})
 
 // test rootDir setting with folder exist
-console.log('The 13th test: custom rootDir as exist folder')
-envAligner({rootDir: 'frontend'})
+// console.log('The 13th test: custom rootDir as exist folder')
+// envAligner({rootDir: 'frontend'})
 
 // test rootDir setting with deep folder exist
-console.log('The 14th test: custom rootDir as deep exist folder')
-envAligner({rootDir: 'application/project/src'})
+// console.log('The 14th test: custom rootDir as deep exist folder')
+// envAligner({rootDir: 'application/project/src'})
 
 // test rootDir setting with same name but at different level
-console.log('The 15th test: custom rootDir as same name but at different level')
-envAligner({rootDir: 'frontend'})
-envAligner({rootDir: 'application/project/frontend'})
+// console.log('The 15th test: custom rootDir as same name but at different level')
+// envAligner({rootDir: 'frontend'})
+// envAligner({rootDir: 'application/frontend'})
 
 // test if schema and env file not at same level
-console.log('The 16th test: custom rootDir as same name but at different level')
-const test16names = {
-  schemaName: '.env.sample',
-  envName: '.env'
-}
+// console.log('The 16th test: custom rootDir as same name but at different level')
+// const test16names = {
+//   schemaName: '.env.sample',
+//   envName: '.env'
+// }
+// envAligner({fileNames: test16names})
 
 // test if schema not exist
-console.log('The 17th test: custom schema file not exist')
-const test17names = {
-  schemaName: '.env.not_exist',
-  envName: '.env'
-}
-envAligner({fileNames: test17names})
+// console.log('The 17th test: custom schema file not exist')
+// const test17names = {
+//   schemaName: '.env.not_exist',
+//   envName: '.env'
+// }
+// envAligner({fileNames: test17names})
 
 // test if env not exist
-console.log('The 18th test: custom env file not exist')
-const test18names = {
-  schemaName: '.env.sample',
-  envName: '.env.not_exist'
-}
-envAligner({fileNames: test18names})
+// console.log('The 18th test: custom env file not exist')
+// const test18names = {
+//   schemaName: '.env.sample',
+//   envName: '.env.not_exist'
+// }
+// envAligner({fileNames: test18names})
 
 // test if schema and env file not exist
 console.log('The 19th test: custom schema and env file not exist')
